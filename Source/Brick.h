8c2abd4ea8d3acfe78400caaa505fbd65e6716c6
Flavien74 <107756028+Flavien74@users.noreply.github.com>
2024-12-18 08:30:00 +0100

    FIX loadTexture call

--- Source/Brick.h
+++ Source/Brick.h
@@ -7,7 +7,7 @@ class Brick : public Entity
 {
 public:
 	Brick() : Entity(), m_Width(DEFAULT_BRICK_WIDTH), m_Height(DEFAULT_BRICK_HEIGHT), m_IsReachable(false) {}
-	Brick(float x, float y) : Entity(x, y), m_Width(DEFAULT_BRICK_WIDTH), m_Height(DEFAULT_BRICK_HEIGHT), m_IsReachable(false) { LoadTexture(TEXTUREBALL); SetSpritePos(x, y); }
+	Brick(float x, float y) : Entity(x, y), m_Width(DEFAULT_BRICK_WIDTH), m_Height(DEFAULT_BRICK_HEIGHT), m_IsReachable(false) { /*LoadTexture(TEXTUREBALL);*/ SetSpritePos(x, y); }
 	Brick(float x, float y, float width, float height) : Entity(x, y), m_Width(width), m_Height(height), m_IsReachable(false) {}
 	~Brick() {}
 
